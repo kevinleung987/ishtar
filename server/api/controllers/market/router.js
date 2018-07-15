0453aeb88c7a1c4e815725691165a2b427bc126a
kevinleung987 <kevin.leung987@hotmail.com>
2018-07-15 16:31:24 -0400

    update package.json, remove test route

--- server/api/controllers/market/router.js
+++ server/api/controllers/market/router.js
@@ -1,4 +1,3 @@
-import request from 'request-promise';
 import * as express from 'express';
 import controller from './controller';
 
@@ -6,18 +5,4 @@ import controller from './controller';
 export default express
   .Router()
   .get('/price/name', controller.priceByName)
-  .get('/price/id', controller.priceById)
-  .get('/test', (req, res) => { // Test for event loop blocking
-    const requestList = [];
-    for (let i = 0; i < 5; i += 1) {
-      const options = {
-        method: 'GET',
-        uri: 'http://localhost:4000',
-        qs: {
-          id: i,
-        },
-      };
-      requestList.push(request(options).then(resp => JSON.parse(resp).id));
-    }
-    Promise.all(requestList).test().then(r => res.json(r));
-  });
+  .get('/price/id', controller.priceById);
