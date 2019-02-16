15b3138d573f1859f102d3ff8697a2640180456d
kevinleung987 <kevin.leung987@hotmail.com>
2019-02-16 17:34:59 -0500

    Rewrite: Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { Client } from 'discord.js';
+import { config } from 'dotenv';
+
+const dotenv = config();
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.TOKEN);
