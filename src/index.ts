0a47fb003b092817edaf286858b073ddcab3ebda
kevinleung987 <kevin.leung987@hotmail.com>
2019-02-18 16:28:24 -0500

    Add Heroku containerized deployment

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { Client } from 'discord.js';
 import { config } from 'dotenv';
-
+import * as express from 'express';
+// Load environment configuration
 const dotenv = config();
+if (dotenv.error) {
+  throw dotenv.error;
+}
+const PORT = process.env.PORT || 3000;
+
 const client = new Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('ready', () => { console.log(`Logged in as ${client.user.tag}!`); });
 
 client.on('message', (msg) => {
   if (msg.content === 'ping') {
@@ -15,3 +19,7 @@ client.on('message', (msg) => {
 });
 
 client.login(process.env.TOKEN);
+
+express()
+.get('/', (req, res) => { res.json({ message: 'nice!' }); })
+.listen(PORT, () => console.log(`Listening on ${PORT}`));
