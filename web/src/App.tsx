c883c3dddd732a83fc76232808ec3b8e904c25c4
kevinleung987 <kevin.leung987@hotmail.com>
2019-04-21 14:40:10 -0400

    Add React to web front-end

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className='App'>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
